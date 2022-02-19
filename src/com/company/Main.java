fd7d217aeb78803d105b3c23c7b0cad6751fcda9
Masha Onofriychuk <97937546+Saishaity@users.noreply.github.com>
2022-02-19 21:12:38 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int[] a = {1, 2, 3, 4, 5, 6, 7};
+	int[] a = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
     int n = 5;
     int f =-1;
     for(int i=0; i<a.length; i++){
